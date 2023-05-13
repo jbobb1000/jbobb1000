0eb10a9daa641462e3139f662bcc370758bb78c3
jbobb1000 <102144587+jbobb1000@users.noreply.github.com>
2023-05-13 12:24:21 +0800

    Update 校园网.js

--- 校园网.js
+++ 校园网.js
@@ -1,11 +1,4 @@
 fetch('https://wxrz.ouc.edu.cn/drcom/chkstatus?callback=dr1002&jsVersion=4.1&v=3965&lang=zh')
   .then(response => response.text())
-  .then(data => {
-    const regex = /\((.*?)\)/; // 正则表达式匹配括号内的内容
-    const match = data.match(regex);
-    if (match) {
-      const contentInParentheses = match[1]; // 获取匹配到的第一个括号内的内容
-      const dictionary = JSON.parse(contentInParentheses); // 将内容解析为字典（对象）
-      const ip = dictionary.v4ip; // 获取v4ip键对应的值，保存在常量ip中
 
 

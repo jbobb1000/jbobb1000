179766ea733a70c4af8c6be10a1674ffc7038019
jbobb1000 <102144587+jbobb1000@users.noreply.github.com>
2023-05-13 12:26:11 +0800

    Update 校园网.js

--- 校园网.js
+++ 校园网.js
@@ -1,4 +1,11 @@
-fetch('https://wxrz.ouc.edu.cn/drcom/chkstatus?callback=dr1002&jsVersion=4.1&v=3965&lang=zh')
-  .then(response => response.text())
+var xhr = new XMLHttpRequest();
+var url = 'https://wxrz.ouc.edu.cn/drcom/chkstatus?callback=dr1002&jsVersion=4.1&v=3965&lang=zh';
 
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log(xhr.responseText);
+  }
+};
 
+xhr.open('GET', url, true);
+xhr.send();
